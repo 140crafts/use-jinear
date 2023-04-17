920f6c5385209de2c65b04023080f0b78d02e887
Cagdas Tunca <cgdstnc@gmail.com>
2023-04-17 19:33:00 +0300

    service worker change

--- jinear-web/next.config.js
+++ jinear-web/next.config.js
@@ -8,7 +8,7 @@ const { withSentryConfig } = require("@sentry/nextjs");
 
 const withPWA = require("next-pwa")({
   dest: "public",
-  importScripts: ["https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js"],
+  importScripts: ["/external-sws/OneSignalSDKWorker.js"],
 });
 
 const nextConfig = {
