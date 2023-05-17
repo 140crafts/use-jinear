07b019d9d1297d629eeea3c068ede9ba55eafc16
Cagdas Tunca <cgdstnc@gmail.com>
2023-05-17 22:12:40 +0300

    feature/cgds-156 adds retrieveTeamNotifications

--- jinear-core/src/main/java/co/jinear/core/repository/NotificationEventRepository.java
+++ jinear-core/src/main/java/co/jinear/core/repository/NotificationEventRepository.java
@@ -14,6 +14,8 @@ public interface NotificationEventRepository extends JpaRepository<NotificationE
 
     Page<NotificationEvent> findAllByWorkspaceIdAndAccountIdAndPassiveIdIsNullOrderByCreatedDateDesc(String workspaceId, String accountId, Pageable pageable);
 
+    Page<NotificationEvent> findAllByWorkspaceIdAndTeamIdAndAccountIdAndPassiveIdIsNullOrderByCreatedDateDesc(String workspaceId, String teamId, String accountId, Pageable pageable);
+
     Long countAllByWorkspaceIdAndAccountIdAndIsReadAndPassiveIdIsNull(String workspaceId, String accountId, Boolean isRead);
 
     @Modifying(flushAutomatically = true, clearAutomatically = true)
@@ -26,5 +28,23 @@ public interface NotificationEventRepository extends JpaRepository<NotificationE
                         notificationEvent.createdDate < :before and 
                         notificationEvent.passiveId is null
                 """)
-    void updateAllBeforeAsRead(@Param("accountId") String accountId, @Param("workspaceId") String workspaceId, @Param("before") Date before);
+    void updateAllBeforeAsRead(@Param("accountId") String accountId,
+                               @Param("workspaceId") String workspaceId,
+                               @Param("before") Date before);
+
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("""
+            update NotificationEvent notificationEvent
+                set notificationEvent.isRead = true, notificationEvent.lastUpdatedDate = current_timestamp() 
+                    where 
+                        notificationEvent.accountId = :accountId and 
+                        notificationEvent.workspaceId = :workspaceId and 
+                        notificationEvent.teamId = :teamId and 
+                        notificationEvent.createdDate < :before and 
+                        notificationEvent.passiveId is null
+                """)
+    void updateAllFromTeamBeforeAsRead(@Param("accountId") String accountId,
+                                       @Param("workspaceId") String workspaceId,
+                                       @Param("teamId") String teamId,
+                                       @Param("before") Date before);
 }
