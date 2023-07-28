9ff76a1c937007a79cdf9ca69611ca6caa62b182
Cagdas Tunca <cgdstnc@gmail.com>
2023-07-29 00:11:21 +0300

    feature/cgds-216

--- jinear-web/worker/index.js
+++ jinear-web/worker/index.js
@@ -1,4 +1,4 @@
 "use strict";
 // importScripts("https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js");
 // importScripts("http://localhost:3000/firebase-messaging-sw.js");
-importScripts("https://jinear.com/firebase-messaging-sw.js");
+importScripts("https://jinear.co/firebase-messaging-sw.js");
