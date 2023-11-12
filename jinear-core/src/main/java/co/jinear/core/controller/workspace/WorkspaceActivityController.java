c80cc5e019b993f24882fc1db8e34b801c5966c8
Cagdas Tunca <cgdstnc@gmail.com>
2023-11-12 17:56:46 +0300

    feature/cgds-281

--- jinear-core/src/main/java/co/jinear/core/controller/workspace/WorkspaceActivityController.java
+++ jinear-core/src/main/java/co/jinear/core/controller/workspace/WorkspaceActivityController.java
@@ -1,7 +1,6 @@
 package co.jinear.core.controller.workspace;
 
 import co.jinear.core.manager.workspace.WorkspaceActivityFilterManager;
-import co.jinear.core.manager.workspace.WorkspaceActivityManager;
 import co.jinear.core.model.request.workspace.WorkspaceActivityFilterRequest;
 import co.jinear.core.model.response.workspace.WorkspaceActivityListResponse;
 import jakarta.validation.Valid;
@@ -14,35 +13,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "v1/workspace/activity")
 public class WorkspaceActivityController {
 
-    private final WorkspaceActivityManager workspaceActivityManager;
     private final WorkspaceActivityFilterManager workspaceActivityFilterManager;
 
     @PostMapping("/filter")
     @ResponseStatus(HttpStatus.OK)
     public WorkspaceActivityListResponse filter(@Valid @RequestBody WorkspaceActivityFilterRequest workspaceActivityFilterRequest) {
         return workspaceActivityFilterManager.filterActivities(workspaceActivityFilterRequest);
-    }
-
-    @GetMapping("/{workspaceId}")
-    @ResponseStatus(HttpStatus.OK)
-    public WorkspaceActivityListResponse retrieveActivities(@PathVariable String workspaceId,
-                                                            @RequestParam(required = false, defaultValue = "0") Integer page) {
-        return workspaceActivityManager.retrieveWorkspaceActivities(workspaceId, page);
-    }
-
-    @GetMapping("/{workspaceId}/team/{teamId}")
-    @ResponseStatus(HttpStatus.OK)
-    public WorkspaceActivityListResponse retrieveActivitiesFromTeam(@PathVariable String workspaceId,
-                                                                    @PathVariable String teamId,
-                                                                    @RequestParam(required = false, defaultValue = "0") Integer page) {
-        return workspaceActivityManager.retrieveWorkspaceTeamActivities(workspaceId, teamId, page);
-    }
-
-    @GetMapping("/{workspaceId}/task/{taskId}")
-    @ResponseStatus(HttpStatus.OK)
-    public WorkspaceActivityListResponse retrieveActivitiesFromTask(@PathVariable String workspaceId,
-                                                                    @PathVariable String taskId,
-                                                                    @RequestParam(required = false, defaultValue = "0") Integer page) {
-        return workspaceActivityManager.retrieveTaskActivities(taskId, page);
     }
 }
--- jinear-core/src/main/java/co/jinear/core/service/workspace/activity/WorkspaceActivityRetrieveService.java
+++ jinear-core/src/main/java/co/jinear/core/service/workspace/activity/WorkspaceActivityRetrieveService.java
@@ -3,29 +3,21 @@ package co.jinear.core.service.workspace.activity;
 import co.jinear.core.converter.workspace.WorkspaceActivityConverter;
 import co.jinear.core.exception.NotFoundException;
 import co.jinear.core.model.dto.workspace.WorkspaceActivityDto;
-import co.jinear.core.model.vo.workspace.RetrieveWorkspaceActivityPageVo;
 import co.jinear.core.repository.WorkspaceActivityRepository;
 import co.jinear.core.system.NormalizeHelper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class WorkspaceActivityRetrieveService {
 
-    private static final int PAGE_SIZE = 25;
-    private static final int TASK_ACTIVITIES_PAGE_SIZE = 125;
-
     private final WorkspaceActivityRepository workspaceActivityRepository;
     private final WorkspaceActivityConverter workspaceActivityConverter;
-    private final WorkspaceActivityDetailRetrieveService workspaceActivityDetailRetrieveService;
 
     public WorkspaceActivityDto retrieveWorkspaceLatestActivity(String workspaceId, List<String> teamIdList) {
         log.info("Retrieve workspace latest activity has started. workspaceId: {}, teamIdList: {}", workspaceId, NormalizeHelper.listToString(teamIdList));
@@ -33,22 +25,4 @@ public class WorkspaceActivityRetrieveService {
                 .map(workspaceActivityConverter::map)
                 .orElseThrow(NotFoundException::new);
     }
-
-    public Page<WorkspaceActivityDto> retrieveWorkspaceActivities(RetrieveWorkspaceActivityPageVo retrieveWorkspaceActivityPageVo) {
-        log.info("Retrieve workspace activities has started. retrieveWorkspaceActivityPageVo: {}", retrieveWorkspaceActivityPageVo);
-        PageRequest pageRequest = Optional.of(retrieveWorkspaceActivityPageVo)
-                .map(RetrieveWorkspaceActivityPageVo::getPage)
-                .map(page -> PageRequest.of(page, PAGE_SIZE))
-                .orElse(PageRequest.of(0, PAGE_SIZE));
-        return workspaceActivityRepository.findAllByWorkspaceIdAndTeamIdIsInAndPassiveIdIsNullOrderByCreatedDateDesc(retrieveWorkspaceActivityPageVo.getWorkspaceId(), retrieveWorkspaceActivityPageVo.getTeamIdList(), pageRequest)
-                .map(workspaceActivityDetailRetrieveService::retrieveDetailsAndMap);
-    }
-
-    public Page<WorkspaceActivityDto> retrieveTaskActivities(String taskId, int page) {
-        log.info("Retrieve task activities has started. taskId: {}, page: {}", taskId, page);
-        PageRequest pageRequest = PageRequest.of(page, TASK_ACTIVITIES_PAGE_SIZE);
-        return workspaceActivityRepository.findAllByTaskIdAndPassiveIdIsNullOrderByCreatedDateDesc(taskId, pageRequest)
-                .map(workspaceActivityDetailRetrieveService::retrieveDetailsAndMap);
-    }
-
 }
