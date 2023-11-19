d992db72b883c36bc09b3a7ff0537f48c16ed4ee
Cagdas Tunca <cgdstnc@gmail.com>
2023-11-19 16:11:57 +0300

    feature/cgds-302 stuff

--- jinear-web/store/api/googleOAuthApi.ts
+++ jinear-web/store/api/googleOAuthApi.ts
@@ -4,7 +4,7 @@ import { api } from "./api";
 export const googleOAuthApi = api.injectEndpoints({
   endpoints: (build) => ({
     retrieveLoginRedirectInfo: build.query<AuthRedirectInfoResponse, void>({
-      query: (req) => `v1/oauth/google/login-redirect-info`,
+      query: (req) => `v1/oauth/google/redirect-info/login`,
       providesTags: (_result, _err, req) => [
         {
           type: "oauth/google/login-redirect-info",
