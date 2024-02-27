0431b5e7dec9c23a7185c5dbab1e4381b88ca2f0
Cagdas Tunca <cgdstnc@gmail.com>
2024-02-27 08:54:20 +0300

    update gitlab-ci

--- jinear-webview-mobile/App.tsx
+++ jinear-webview-mobile/App.tsx
@@ -33,7 +33,7 @@ const USER_AGENT =
     ? "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3 Safari/605.1.15"
     : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36";
 
-const URL = "http://localhost:3000";
+const URL = __DEV__ ? "http://localhost:3000" : "https://jinear.co";
 // const URL = "https://jinear.co";
 
 export default function App() {
