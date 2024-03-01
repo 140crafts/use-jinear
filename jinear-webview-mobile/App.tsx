a8400f0d4011bc9727375c3cad2a9157906e94b6
Cagdas Tunca <cgdstnc@gmail.com>
2024-03-01 17:09:53 +0300

    stuff

--- jinear-webview-mobile/App.tsx
+++ jinear-webview-mobile/App.tsx
@@ -1,11 +1,18 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
+import Constants from "expo-constants";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 import { StatusBar, StatusBarStyle } from "expo-status-bar";
 import { useEffect, useRef, useState } from "react";
 import { BackHandler, Dimensions, Platform, SafeAreaView, StyleSheet, TouchableOpacity, View } from "react-native";
 import WebView, { WebViewMessageEvent } from "react-native-webview";
-
 import { ShouldStartLoadRequest } from "react-native-webview/lib/WebViewTypes";
 import Logger from "./utils/logger";
+
+interface IRetrieveExpoPushTokenResult {
+  permissionNeeded?: boolean;
+  tokenData?: string;
+}
 
 const logger = Logger("App");
 
@@ -13,7 +20,7 @@ const SCREEN_WIDTH = Dimensions.get("window").width;
 const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 const DARK_THEME = {
-  current: "dark" as "light" | "dark",
+  current: "light" as "light" | "dark",
   statusBarStyle: "light" as StatusBarStyle,
   backgroundColor: "#16171a",
   backgroundColorModalBackdrop: "#09090A",
@@ -21,7 +28,7 @@ const DARK_THEME = {
 };
 
 const LIGHT_THEME = {
-  current: "light" as "light" | "dark",
+  current: "dark" as "light" | "dark",
   statusBarStyle: "dark" as StatusBarStyle,
   backgroundColor: "#fdfffc",
   backgroundColorModalBackdrop: "#656665",
@@ -33,8 +40,65 @@ const USER_AGENT =
     ? "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3 Safari/605.1.15"
     : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36";
 
-const URL = __DEV__ ? "http://localhost:3000" : "https://jinear.co";
-// const URL = "https://jinear.co";
+// const URL = __DEV__ ? "http://localhost:3000" : "https://jinear.co";
+const URL = "https://jinear.co";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+function setNotificationChannel() {
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+}
+
+const retrieveExpoPushToken = async (): Promise<IRetrieveExpoPushTokenResult | undefined> => {
+  setNotificationChannel();
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    if (existingStatus == "granted") {
+      const token = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants?.expoConfig?.extra?.eas.projectId,
+      });
+      return { tokenData: token.data };
+    }
+    return { permissionNeeded: true };
+  }
+};
+
+const askNotificationPermissions = async () => {
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  if (existingStatus !== "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    return status;
+  }
+  return existingStatus;
+};
+
+function getInjectableJSMessage(message: any) {
+  return `
+    (function() {
+      document.dispatchEvent(new MessageEvent('app-message', {
+        data: ${JSON.stringify(message)}
+      }));
+    })();
+  `;
+}
+
+interface IPushWebViewMessage {
+  method: "pushNotificationTokenResulted" | "pushNotificationStateResulted";
+  payload?: any;
+}
 
 export default function App() {
   const webViewRef = useRef<WebView>(null);
@@ -63,6 +127,7 @@ export default function App() {
 
   const onWebViewMessage = (event: WebViewMessageEvent) => {
     const message = JSON.parse(event.nativeEvent.data);
+    logger.log({ message });
     switch (message.method) {
       case "themeChange":
         onThemeChangeMessage(message.payload);
@@ -70,18 +135,27 @@ export default function App() {
       case "modalVisible":
         onModalVisibleChangeMessage(message.payload);
         break;
+      case "pushNotificationTokenRequest":
+        onPushNotificationTokenRequest();
+      case "askPermissionsAndSendToken":
+        onAskPermissionsAndSendToken();
+      case "removePushNotificationTokenRequest":
+        onRemovePushNotificationTokenRequest();
       default:
         break;
     }
   };
 
+  const pushWebViewMessage = (message: IPushWebViewMessage) => {
+    logger.log({ pushWebViewMessage: message });
+    webViewRef.current?.injectJavaScript(getInjectableJSMessage(message));
+  };
+
   const onThemeChangeMessage = (theme: string) => {
-    logger.log({ onThemeChangeMessage: theme });
     setTheme(theme == "light" ? LIGHT_THEME : DARK_THEME);
   };
 
   const onModalVisibleChangeMessage = (visible: boolean) => {
-    logger.log({ onModalVisibleChangeMessage: visible });
     setTheme((theme) => {
       return {
         ...theme,
@@ -96,6 +170,23 @@ export default function App() {
     });
   };
 
+  const onPushNotificationTokenRequest = async () => {
+    const result = await retrieveExpoPushToken();
+    pushWebViewMessage({ method: "pushNotificationTokenResulted", payload: { result } });
+  };
+
+  const onAskPermissionsAndSendToken = async () => {
+    const status = await askNotificationPermissions();
+    if (status == "granted") {
+      const result = await retrieveExpoPushToken();
+      pushWebViewMessage({ method: "pushNotificationTokenResulted", payload: { result } });
+    }
+  };
+
+  const onRemovePushNotificationTokenRequest = async () => {
+    await Notifications.unregisterForNotificationsAsync();
+  };
+
   const loadingHandler = (syntheticEvent: any) => {
     const loading = syntheticEvent?.nativeEvent?.loading;
     const isExternal = syntheticEvent.nativeEvent.url?.indexOf(URL) == -1;
@@ -107,9 +198,7 @@ export default function App() {
 
   const _onShouldStartLoadWithRequest = (event?: ShouldStartLoadRequest) => {
     const isInternal = event?.url?.indexOf(URL) != -1;
-    logger.log({ on: "onShouldStartLoadWithRequest", isInternal: isInternal, url: event?.url });
     if (isInternal) {
-      logger.log({ setLastAppUrl: event?.url });
       setLastAppUrl(event?.url);
     } else {
       setShowWebViewNavBar(!isInternal);
