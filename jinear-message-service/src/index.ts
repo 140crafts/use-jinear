f2d3c5443384a600da90c2ff7e5ad9abd6cd110c
Cagdas Tunca <cgdstnc@gmail.com>
2024-05-13 22:34:09 +0300

    main stuff

--- jinear-message-service/src/index.ts
+++ jinear-message-service/src/index.ts
@@ -12,9 +12,10 @@ const {combine, timestamp, json, errors} = winston.format;
 
 const {
     PORT = 3001,
-    INTERNAL_KEY = 'debug',
+    INTERNAL_AUTH_TOKEN = 'debug',
     AXIOM_TOKEN = '',
-    AXIOM_ORG_ID = ''
+    AXIOM_ORG_ID = '',
+    CORE_ENDPOINT = 'http://localhost:8085'
 } = process.env;
 
 const logger = winston.createLogger({
@@ -49,6 +50,48 @@ app.use(express.json(), morganMiddleware)
 let http = require("http").Server(app);
 let io = require("socket.io")(http, {path: '/ws'});
 
+interface IParsedCookie {
+    name: string;
+    value: string
+}
+
+const parseCookie = (cookie?: string) => {
+    return cookie?.split(';').map((pair) => {
+        const indexOfEquals = pair.indexOf('=');
+        let name;
+        let value;
+        if (indexOfEquals === -1) {
+            name = '';
+            value = pair.trim();
+        } else {
+            name = pair.substr(0, indexOfEquals).trim();
+            value = pair.substr(indexOfEquals + 1).trim();
+        }
+        const firstQuote = value.indexOf('"');
+        const lastQuote = value.lastIndexOf('"');
+        if (firstQuote !== -1 && lastQuote !== -1) {
+            value = value.substring(firstQuote + 1, lastQuote);
+        }
+        return {name, value};
+    });
+}
+
+const retrieveJWT = (cookies?: IParsedCookie[]) => {
+    return cookies?.find(value => value.name == "JWT")
+}
+
+const getAccountId = async (jwt: string): Promise<String> => {
+    const response = await fetch(`${CORE_ENDPOINT}/v1/account`, {
+        "headers": {
+            "accept": "*/*",
+            "Authorization": `Bearer ${jwt}`
+        },
+        "method": "GET"
+    });
+    const body = await response.json();
+    return body.data.accountId;
+}
+
 app.get('/', (req, resp) => {
     const cookie = req.headers.cookie;
     logger.log({level: 'info', message: `cookie: ${cookie}`});
@@ -57,38 +100,42 @@ app.get('/', (req, resp) => {
 })
 
 app.get('/info', (req, resp) => {
-    let srvSockets = io.sockets.sockets;
     let rooms = io.sockets.adapter.rooms;
-    console.log({srvSockets, rooms, ...req.body})
-    return resp.status(200).send({srvSockets, rooms});
+    logger.info({reqIp: req.ip, rooms: Array.from(rooms.entries())});
+    return resp.status(200).send('logs sent');
 })
 
 app.post('/emit', (req, resp) => {
-    console.log({...req.body, keyValid: (req?.body?.key == INTERNAL_KEY)})
-    let {key, channel, topic, message} = req.body;
-
-    if (key != INTERNAL_KEY) {
-        return resp.status(404);
+    const authToken = req.headers.authorization?.split("Bearer ")?.[1];
+    let {channel, topic, message} = req.body;
+    const tokenValid = authToken == INTERNAL_AUTH_TOKEN;
+    logger.info({tokenValid, channel, topic, message});
+    if (!tokenValid) {
+        return resp.status(401).send('Access denied');
     }
     try {
         io.to(channel).emit(topic, message);
     } catch (error) {
-        console.log({error})
+        logger.error(error);
     }
     return resp.status(200).send("ok");
 })
 
 io.on('connection', async (socket: any) => {
-    console.log({headers: socket?.handshake?.headers})
-    // const accId = await getAccId(socket?.handshake?.headers?.cookie);
-    // if (!accId) {
-    //     socket.disconnect();
-    //     return;
-    // }
-    socket.join('accId');
-    // socket.emit("msg", `joined room ${accId}`);
+    const cookie = parseCookie(socket?.handshake?.headers?.cookie);
+    const JWT = retrieveJWT(cookie);
+    if (JWT) {
+        try {
+            const accountId = await getAccountId(JWT.value);
+            logger.info({newConnection: accountId});
+            socket.join(accountId);
+            return;
+        } catch (e) {
+            logger.error(e)
+        }
+    }
+    socket.disconnect();
 });
-
 
 http.listen(PORT, function () {
     console.log(`listening on *:${PORT}`);
