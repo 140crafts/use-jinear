201037e4bc906f2890b345916d5153691b62dadd
Cagdas Tunca <cgdstnc@gmail.com>
2024-05-29 07:53:52 +0300

    feature/jin-474 stuff

--- jinear-web/components/conversationsSectionSideMenu/conversationList/ConversationList.tsx
+++ jinear-web/components/conversationsSectionSideMenu/conversationList/ConversationList.tsx
@@ -4,7 +4,7 @@ import MenuGroupTitle from "@/components/sideMenu/menuGroupTitle/MenuGroupTitle"
 import CircularLoading from "@/components/circularLoading/CircularLoading";
 import useTranslation from "@/locals/useTranslation";
 import { useRetrieveParticipatedConversationsQuery } from "@/api/conversationApi";
-import { WorkspaceDto } from "@/be/jinear-core";
+import { ConversationParticipantDto, WorkspaceDto } from "@/be/jinear-core";
 import Logger from "@/utils/logger";
 import ConversationButton
   from "@/components/conversationsSectionSideMenu/conversationList/conversationButton/ConversationButton";
@@ -12,7 +12,8 @@ import { useTypedSelector } from "@/store/store";
 import { selectCurrentAccountId } from "@/slice/accountSlice";
 import NewConversationButton
   from "@/components/conversationsSectionSideMenu/conversationList/newConversationButton/NewConversationButton";
-import { selectConversationLastCheck, selectConversationLastCheckMap } from "@/slice/messagingSlice";
+import { useLiveQuery } from "dexie-react-hooks";
+import { getSortedConversationIds } from "../../../repository/MessageRepository";
 
 interface ConversationListProps {
   workspace: WorkspaceDto;
@@ -27,18 +28,18 @@ const ConversationList: React.FC<ConversationListProps> = ({ workspace }) => {
     data: participatedConversationsResponse,
     isLoading
   } = useRetrieveParticipatedConversationsQuery({ workspaceId: workspace.workspaceId });
-  const conversationLastCheckMap = useTypedSelector(selectConversationLastCheckMap(workspace.workspaceId));
+  const distinctSortedConversationIds = useLiveQuery(() => getSortedConversationIds());
 
   const sortedParticipatedConversations = useMemo(() => {
-    if (conversationLastCheckMap && participatedConversationsResponse) {
-      const mergedData = participatedConversationsResponse.data.map((conversationParticipantDto) => {
-        const localLastCheck = conversationLastCheckMap[conversationParticipantDto.conversationId] ? conversationLastCheckMap[conversationParticipantDto.conversationId] : new Date();
-        return { conversationParticipantDto: conversationParticipantDto, localLastCheck };
-      });
-      return mergedData.sort((a, b) => new Date(a.localLastCheck).getTime() - new Date(b.localLastCheck).getTime()).map(val => val.conversationParticipantDto);
+    const participatedConversations = participatedConversationsResponse?.data;
+    if (distinctSortedConversationIds && participatedConversations) {
+      return distinctSortedConversationIds
+        .map(conversationId =>
+          participatedConversations.find(participatedConversation => participatedConversation.conversationId == conversationId) || -1)
+        .filter(participantDto => participantDto != -1) as ConversationParticipantDto[];
     }
     return [];
-  }, [participatedConversationsResponse, conversationLastCheckMap]);
+  }, [participatedConversationsResponse, distinctSortedConversationIds]);
 
   return (
     <div className={styles.container}>
@@ -63,7 +64,6 @@ const ConversationList: React.FC<ConversationListProps> = ({ workspace }) => {
           <NewConversationButton workspaceId={workspace.workspaceId} workspaceName={workspace.username} />
         </div>
       </>}
-
     </div>);
 };
 
