b881676cc8e112ddb1314cc6bee5a70db863a856
Cagdas Tunca <cgdstnc@gmail.com>
2025-05-12 21:15:33 +0300

    stuff

--- jinear-pages/components/postHogProvider/CSPostHogProvider.js
+++ jinear-pages/components/postHogProvider/CSPostHogProvider.js
@@ -1,14 +1,23 @@
 "use client";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
+import { env } from "next-runtime-env";
+import Logger from "@/utils/logger";
+
+const logger = Logger("CSPostHogProvider");
 
 if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-    capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-    capture_pageleave: true // Enable pageleave capture
-  });
+  const NEXT_PUBLIC_POSTHOG_KEY = env("NEXT_PUBLIC_POSTHOG_KEY");
+  const NEXT_PUBLIC_POSTHOG_HOST = env("NEXT_PUBLIC_POSTHOG_HOST");
+  if (NEXT_PUBLIC_POSTHOG_KEY && NEXT_PUBLIC_POSTHOG_HOST) {
+    logger.log(`Initializing postHog. NEXT_PUBLIC_POSTHOG_KEY: ${NEXT_PUBLIC_POSTHOG_KEY}, NEXT_PUBLIC_POSTHOG_HOST: ${NEXT_PUBLIC_POSTHOG_HOST}`);
+    posthog.init(NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: NEXT_PUBLIC_POSTHOG_HOST,
+      person_profiles: "identified_only",
+      capture_pageview: false,
+      capture_pageleave: true
+    });
+  }
 }
 
 export function CSPostHogProvider({ children }) {
