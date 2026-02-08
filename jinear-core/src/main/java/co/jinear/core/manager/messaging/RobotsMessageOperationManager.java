67f0b1ebb088b5ee25bc749368cba3b04a44a6d5
Cagdas Tunca <cgdstnc@gmail.com>
2026-02-08 19:51:30 +0300

    Refactor RobotsMessageOperationManager by removing unused imports and optimizing code structure

--- jinear-core/src/main/java/co/jinear/core/manager/messaging/RobotsMessageOperationManager.java
+++ jinear-core/src/main/java/co/jinear/core/manager/messaging/RobotsMessageOperationManager.java
@@ -1,27 +1,20 @@
 package co.jinear.core.manager.messaging;
 
 import co.jinear.core.converter.messaging.message.SendMessageRequestConverter;
-import co.jinear.core.exception.BusinessException;
-import co.jinear.core.exception.NotValidException;
 import co.jinear.core.model.dto.messaging.message.RichMessageDto;
 import co.jinear.core.model.request.messaging.message.SendMessageRequest;
 import co.jinear.core.model.response.BaseResponse;
-import co.jinear.core.model.vo.captcha.CaptchaResolveVo;
 import co.jinear.core.model.vo.messaging.message.InitializeMessageVo;
 import co.jinear.core.service.SessionInfoService;
 import co.jinear.core.service.captcha.CaptchaChallengeService;
 import co.jinear.core.service.messaging.message.MessageOperationService;
 import co.jinear.core.service.messaging.message.MessageRetrieveService;
 import co.jinear.core.service.messaging.thread.ThreadNotifierService;
-import co.jinear.core.system.IpResolver;
 import co.jinear.core.validator.messaging.thread.ThreadAccessValidator;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Objects;
 
 @Slf4j
 @Service
